ff0d41f1bb819f5b7974a94a551737cb8c4bab04
Vladimir Apostolski <vld_apo@yahoo.com>
2020-09-19 10:44:50 +0200

    added startup-01 template

---
+++ landing-pages/startup-01/tailwind.config.js
@@ -0,0 +1,14 @@
+// use: npx tailwindcss build styles/tailwind.css -o styles/style.css
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
